b566ae9dee593fe1a591692019a79acfb2f2a9db
Andrey <patron6239@gmail.com>
2022-10-21 20:54:15 +0300

    Change home work 1 and home work 2

--- Home work/src/HomeWork1.java
+++ Home work/src/HomeWork1.java
@@ -1,8 +1,9 @@
 import java.util.*;
+import java.util.logging.Logger;
 
 public class HomeWork1 {
     public static void main(String[] args) {
-        /*
+
         // Вычислить сумму чисел от 1 до n; произведение чисел от 1 до n
         System.out.println("\ntask 1");
         Summ(1, 10);
@@ -18,8 +19,6 @@ public class HomeWork1 {
         System.out.println("\ntask 3");
         Calc(10, 10);
         System.out.println("___________________________________________");
-        */
-
 
         // Задано уравнение вида q + w = e, причем q, w, e >= 0.
         // Некоторые цифры могут быть заменены знаком вопроса, например 2? + ?5 = 69.
@@ -71,25 +70,42 @@ public class HomeWork1 {
     }
 
     public static void Calc(int num1, int num2) {
+        Logger logger = Logger.getAnonymousLogger();
+        logger.info("Получено число: " + num1);
+        logger.info("Получено число: " + num2);
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("""
                 введите 1 - для умножения
-                       2 - для деления
-                       3 - для сложения
-                       4 - для вычитания""");
+                        2 - для деления
+                        3 - для сложения
+                        4 - для вычитания""");
         String act = scanner.next();
 
         switch (act) {
-            case "1" -> System.out.println("result = " + (num1 * num2));
-            case "2" -> System.out.println("result = " + (num1 / num2));
-            case "3" -> System.out.println("result = " + (num1 + num2));
-            case "4" -> System.out.println("result = " + (num1 - num2));
+            case "1" -> {
+                System.out.println("result = " + (num1 * num2));
+                logger.info("(num1 * num2) result = " + (num1 * num2));
+            }
+            case "2" -> {
+                System.out.println("result = " + (num1 / num2));
+                logger.info("(num1 / num2) result = " + (num1 / num2));
+            }
+            case "3" -> {
+                System.out.println("result = " + (num1 + num2));
+                logger.info("(num1 + num2) result = " + (num1 + num2));
+            }
+            case "4" -> {
+                System.out.println("result = " + (num1 - num2));
+                logger.info("(num1 - num2) result = " + (num1 - num2));
+            }
         }
     }
 
     private static String[] ParseInputScanEquation() {
         Scanner scanner = new Scanner(System.in);
         String[] equation = scanner.next().split("");
+
 
         for (int i = 0; i < equation.length; i++) {
             if (equation[i].equals("+") | equation[i].equals("-") | equation[i].equals("=")) {
@@ -98,9 +114,8 @@ public class HomeWork1 {
         }
 
         String equateStr = String.join("", equation);
-        String[] equate = equateStr.split(" ");
 
-        return equate;
+        return equateStr.split(" ");
     }
 
     public static void ResultEquation() {
@@ -110,6 +125,7 @@ public class HomeWork1 {
         ArrayList<String> numberEquate = new ArrayList<String>();
         for (int i = 0; i < equate.length - 1; i++) {
             if (equate[i].equals("+") | equate[i].equals("-") | equate[i].equals("=")) {
+
             } else {
                 numberEquate.add(equate[i]);
             }
--- Home work/src/HomeWork2.java
+++ Home work/src/HomeWork2.java
@@ -1,9 +1,11 @@
+import java.io.File;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 public class HomeWork2 {
     public static void main(String[] args) {
         /*
-        Задание 1 а
+        Задание 1а.
         Дана строка sql-запроса "select * from students where ".
         Сформируйте часть WHERE этого запроса, используя StringBuilder.
         Данные для фильтрации приведены ниже в виде json строки.
@@ -18,25 +20,53 @@ public class HomeWork2 {
         System.out.println(createRequestSelect(startRequestSelect, textRequestSelect));
 
         /*
-        Задание 1 б
+        Задание 1б.
         Написать метод(ы), который распарсит json и, используя StringBuilder,
         создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
         */
 
-        String[] json = new String[]{"{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}", "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}", "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}"};
-        System.out.println(Arrays.toString(json));
+        String[] json = new String[]{"{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}",
+                "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}",
+                "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}"};
 
+        System.out.println(Arrays.toString(json));
         System.out.println(getReportStudents(json));
+
+        /*
+        Задание 2.
+        Реализуйте алгоритм сортировки пузырьком числового массива,
+        результат после каждой итерации запишите в лог-файл.
+         */
+
+        int[] arrayNumber = {1, 5, 15, 10, 7, 25, 3};
+        sortBuble(arrayNumber);
+        System.out.println(Arrays.toString(arrayNumber));
+
+        /*
+        Задание 3.
+        Напишите метод, который определит тип (расширение) файлов
+        из текущей папки и выведет в консоль результат вида
+        1 Расширение файла: txt
+        2 Расширение файла: pdf
+        3 Расширение файла:
+        4 Расширение файла: jpg
+        */
+
+        String dir = "C://Users//Андрей//Desktop//Практические по ВС//java//Java//Home work//src";
+        getTypeFileFromDirectory(dir);
+
     }
 
     public static StringBuilder createRequestSelect(String startRequestSelect, String textRequestSelect) {
         StringBuilder requestSelect = new StringBuilder(startRequestSelect);
 
-        String[] arrayText = (textRequestSelect.replaceAll("[\\{}]", "").replaceAll("[:]", "=")).split(" ");
+        String[] arrayText = (textRequestSelect.replaceAll("[\\{}]", "").
+                replaceAll(":", "=")).
+                split(" ");
 
-        for (int i = 0; i < arrayText.length; i++) {
-            if (!arrayText[i].contains("null")) {
-                requestSelect.append(arrayText[i]);
+        for (String s : arrayText) {
+            if (!s.contains("null")) {
+                requestSelect.append(s);
             }
         }
 
@@ -45,14 +75,54 @@ public class HomeWork2 {
 
     public static StringBuilder getReportStudents(String[] jsonStringList) {
         StringBuilder reportStudents = new StringBuilder();
-        for (int i = 0; i < jsonStringList.length; i++) {
-            String[] arrayTemp = jsonStringList[i].replaceAll("[\\{}]", "").replaceAll("[\\:,]", " ").split(" ");
-            System.out.println(Arrays.toString(arrayTemp));
-            String[] studentReport = new String[] {"Студент " + arrayTemp[1] + " получил " + arrayTemp[3] + " по предмету " + arrayTemp[5]};
+
+        for (String s : jsonStringList) {
+            String[] arrayTemp = s.replaceAll("[\\{}]", "").
+                    replaceAll("[:,]", " ").
+                    split(" ");
+
+            String[] studentReport = new String[]{"Студент " + arrayTemp[1] + " получил " + arrayTemp[3] + " по предмету " + arrayTemp[5]};
             reportStudents.append(Arrays.toString(studentReport));
         }
+
         return reportStudents;
     }
+
+    public static void sortBuble(int[] array) {
+        Logger logger = Logger.getAnonymousLogger();
+
+        for (int i = 0; i < array.length; i++)
+        {
+            for (int j = 0; j < array.length - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    logger.info("temp = " + array[j] + " array[j] = " + array[j + 1] + " array[j + 1] = " + temp);
+                }
+            }
+        }
+    }
+
+    public static void getTypeFileFromDirectory(String directory) {
+        File dir = new File(directory);
+        if(dir.isDirectory())
+        {
+            for(File item : dir.listFiles()){
+                if(item.isDirectory()){
+                    System.out.println("Каталог: " + item.getName());
+                }
+                else{
+                    System.out.println("Файл с расширением: " +  item.getName().substring(item.getName().indexOf(".")));
+                }
+            }
+        }
+    }
+
+
 }
 
 
