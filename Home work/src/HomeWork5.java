7ea9894087d48d22d3ec90709edbf369decbe959
Andrey <patron6239@gmail.com>
2022-11-16 23:37:03 +0300

    Create homework 5 Task 3,4

--- Home work/src/HomeWork5.java
+++ Home work/src/HomeWork5.java
@@ -32,7 +32,21 @@ public class HomeWork5 {
 
         workers.printSortedListWorkers();
 
+        //Задание 3.
+        //Реализовать алгоритм пирамидальной сортировки (HeapSort).
+        QueensChess queensChess = new QueensChess(8);
+        MyThread tr = new MyThread(queensChess, 0);
+        tr.run();
 
+
+        //Задание 3.
+        //Реализовать алгоритм пирамидальной сортировки (HeapSort).
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+
+        HeapSort.heapSort(sortArr);
+        for (int i = 0; i < sortArr.length; i++) {
+            System.out.print(sortArr[i] + "\n");
+        }
     }
 }
 
@@ -286,4 +300,173 @@ class Workers {
         }
         logger.info("Вывод в консоль отсортированного списка выполнен успешно");
     }
+}
+
+class QueensChess {
+    protected int SIZE ;
+    protected int board[][];
+    protected int results_count = 0;
+    private int threadsCount;
+    public QueensChess(){
+    }
+    public QueensChess(int N){
+        SIZE=N;
+        board=new int [N][N];
+        //zan=new boolean [N][N];
+        results_count=0;
+    }
+    public boolean tryQueen(int a, int b){
+        for (int i = 0; i < a; ++i)
+            if (board[i][b]==1)
+                return false;
+        for (int i = 1; i <= a && b - i >= 0; ++i)
+            if (board[a - i][b - i]==1)
+                return false;
+
+        for (int i = 1; i <= a && b + i < SIZE; i++)
+            if (board[a - i][b + i]==1)
+                return false;
+
+        return true;
+    }
+    public void setQueen(int a) throws InterruptedException {
+        for(int i = 0; i < SIZE; ++i){
+            if (tryQueen(a, i)) {
+                board[a][i] = 1;
+                Thread thr = new MyThread(this,a+1);
+                thr.start();
+                thr.join();
+                board[a][i] = 0;
+            }
+        }
+        return;
+    }
+    public  void showBoard(){
+        for (int a = 0; a < SIZE; ++a)
+        {
+            for (int b = 0; b < SIZE; ++b)
+            {
+                System.out.print((board[a][b]==1) ? "X " : "0 ");
+            }
+            System.out.print('\n');
+        }
+        System.out.print('\n');
+    }
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        for (int a = 0; a < SIZE; ++a) {
+            for (int b = 0; b < SIZE; ++b) {
+                if(board[a][b]==1)
+                    builder.append( "Q ");
+                else
+                    builder.append( ". ");
+            }
+        }
+        String completedString = builder.toString();
+        return completedString;
+    }
+}
+class MyThread extends Thread {
+    private volatile QueensChess clock;
+    private volatile int position;
+
+    public MyThread(QueensChess clock1, int pos) {
+        position = pos;
+        clock = clock1;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (position == clock.SIZE) {
+                ++clock.results_count;
+                System.out.print("Result #" + clock.results_count + '\n');
+                clock.showBoard();
+                return;
+            }
+            for (int i = 0; i < clock.SIZE; ++i) {
+                if (clock.tryQueen(position, i)) {
+                    clock.board[position][i] = 1;
+                    Thread thr = new MyThread(clock, position + 1);
+                    thr.start();
+                    thr.join();
+                    clock.board[position][i] = 0;
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class HeapSort {
+    //вернуть левого потомка `A[i]`
+    private static int LEFT(int i) {
+        return (2 * i + 1);
+    }
+
+    //вернуть правого потомка `A[i]`
+    private static int RIGHT(int i) {
+        return (2 * i + 2);
+    }
+
+    //вспомогательная функция для замены двух индексов в массиве
+    private static void swap(int[] sortArr, int i, int j) {
+        int swap = sortArr[i];
+        sortArr[i] = sortArr[j];
+        sortArr[j] = swap;
+    }
+
+    //рекурсивный алгоритм heapify-down. Узел с индексом `i` и 2 его прямых потомка нарушают свойство кучи
+    private static void heapify(int[] sortArr, int i, int size) {
+        // получить левый и правый потомки узла с индексом `i`
+        int left = LEFT(i);
+        int right = RIGHT(i);
+        int largest = i;
+
+        //сравниваем `A[i]` с его левым и правым дочерними элементами и находим наибольшее значение
+        if (left < size && sortArr[left] > sortArr[i]) largest = left;
+        if (right < size && sortArr[right] > sortArr[largest]) largest = right;
+
+        //поменяться местами с потомком, имеющим большее значение и вызовите heapify-down для дочернего элемента
+        if (largest != i) {
+            swap(sortArr, i, largest);
+            heapify(sortArr, largest, size);
+        }
+    }
+
+    //функция для удаления элемента с наивысшим приоритетом (присутствует в корне)
+    public static int pop(int[] sortArr, int size) {
+        //если в куче нет элементов
+        if (size <= 0) {
+            return -1;
+        }
+        int top = sortArr[0];
+
+        //заменяем корень кучи последним элементом массива
+        sortArr[0] = sortArr[size - 1];
+        //вызовите heapify-down на корневом узле
+        heapify(sortArr, 0, size - 1);
+        return top;
+    }
+
+    //функция для выполнения пирамидальной сортировки массива `A` размера `n`
+    public static void heapSort(int[] sortArr) {
+        //строим приоритетную очередь и инициализируем ее заданным массивом
+        int n = sortArr.length;
+
+        //build-heap: вызывать heapify, начиная с последнего внутреннего
+        //узел до корневого узла
+        int i = (n - 2) / 2;
+        while (i >= 0) {
+            heapify(sortArr, i--, n);
+        }
+
+        //несколько раз извлекаем из кучи, пока она не станет пустой
+        while (n > 0) {
+            sortArr[n - 1] = pop(sortArr, n);
+            n--;
+        }
+    }
+
 }
