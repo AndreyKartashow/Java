72a83d1e65e1707585737786bbbbfdf06edacb99
Andrey <patron6239@gmail.com>
2022-11-19 14:22:33 +0300

    Change homework 6 Task 1-4

--- Home work/src/HomeWork6.java
+++ Home work/src/HomeWork6.java
@@ -1,12 +1,9 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class HomeWork6 {
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.runServer();
-
     }
 }
 
@@ -15,16 +12,16 @@ class Notebook {
     private String model;
     private String ram;
     private String memoryHardDisk;
-    private String opetationSystem;
+    private String operationSystem;
     private String processor;
     private String videoCard;
 
-    public Notebook(String manufacturer, String model, String ram, String memoryHardDisk, String opetationSystem, String processor, String videoCard) {
+    public Notebook(String manufacturer, String model, String ram, String memoryHardDisk, String operationSystem, String processor, String videoCard) {
         this.manufacturer = manufacturer;
         this.model = model;
         this.ram = ram;
         this.memoryHardDisk = memoryHardDisk;
-        this.opetationSystem = opetationSystem;
+        this.operationSystem = operationSystem;
         this.processor = processor;
         this.videoCard = videoCard;
     }
@@ -36,28 +33,28 @@ class Notebook {
                 ", model='" + model + '\'' +
                 ", RAM=" + ram +
                 ", memoryHardDisk=" + memoryHardDisk +
-                ", opetationSystem='" + opetationSystem + '\'' +
+                ", opetationSystem='" + operationSystem + '\'' +
                 ", processor='" + processor + '\'' +
                 ", videoCard='" + videoCard + '\'' +
                 '}';
     }
 }
 
-class NotebookCollection{
+class NotebookCollection {
     Map<Notebook, String> mapNotebook;
 
-    NotebookCollection(){
+    NotebookCollection() {
         mapNotebook = new HashMap<>();
     }
 
-    protected void addNotebook(Notebook notebook, String count){
+    protected void addNotebook(Notebook notebook, String count) {
         mapNotebook.put(notebook, count);
     }
 
-    protected void printMapNotebook(){
+    protected void printMapNotebook() {
         Scanner scanner = new Scanner(System.in);
 
-        for (Map.Entry<Notebook, String> set : mapNotebook.entrySet()){
+        for (Map.Entry<Notebook, String> set : mapNotebook.entrySet()) {
             System.out.println(set.getKey() + " - В наличии: " + set.getValue());
         }
 
@@ -66,23 +63,40 @@ class NotebookCollection{
         scanner.nextLine();
     }
 
-    protected Map<Notebook, String> findNotebook(){
-        Scanner scanner = new Scanner(System.in);
+    protected void findNotebook(List<String> parametersSearch) {
+        Map<Notebook, String> treeMapNotebook = new LinkedHashMap<>();
+        int maxCountParameters = parametersSearch.size();
 
+        while (maxCountParameters > 0) {
+            for (Map.Entry<Notebook, String> set : mapNotebook.entrySet()) {
+                int currentCountParameters = 0;
+                for (String parameter : parametersSearch) {
+                    if (set.getKey().toString().contains(parameter)) {
+                        currentCountParameters++;
+                        if (currentCountParameters == maxCountParameters) {
+                            treeMapNotebook.put(set.getKey(), set.getValue());
+                        }
+                    }
+                }
+            }
+            maxCountParameters--;
+        }
 
+        for (Map.Entry<Notebook, String> set : treeMapNotebook.entrySet()) {
+            System.out.println(set.getKey() + " - В наличии: " + set.getValue());
+        }
 
-        return mapNotebook;
     }
 }
 
-class Chop{
-    protected static NotebookCollection initProducts(){
+class Chop {
+    protected static NotebookCollection initProducts() {
         NotebookCollection notebookMap = new NotebookCollection();
 
-        Notebook notebook1 = new Notebook("ASUS", "A456-7", "32Гб", "1Тб", "Windows10", "Intel i5", "NVIDIA 1560");
+        Notebook notebook1 = new Notebook("ASUS", "A456-7", "32Гб", "1Тб", "Windows8", "Intel i5", "NVIDIA 1560");
         Notebook notebook2 = new Notebook("Lenovo", "975-4", "64Гб", "1Тб", "Windows7", "Intel i5", "NVIDIA 1560");
         Notebook notebook3 = new Notebook("Lenovo", "47-4527A", "32Гб", "1Тб", "Windows8", "Intel i5", "NVIDIA 1560");
-        Notebook notebook4 = new Notebook("ASUS", "Super-756X", "32Гб", "1Тб", "Windows10", "Intel i5", "NVIDIA 1560");
+        Notebook notebook4 = new Notebook("ASUS", "Super-756X", "64Гб", "1Тб", "Windows10", "Intel i5", "NVIDIA 1560");
         Notebook notebook5 = new Notebook("Lenovo", "FGN54-561", "32Гб", "1Тб", "Windows8", "Intel i5", "NVIDIA 1560");
         Notebook notebook6 = new Notebook("ASUS", "6452-Atlanta x7", "32Гб", "1Тб", "Windows10", "Intel i5", "NVIDIA 1560");
 
@@ -97,8 +111,8 @@ class Chop{
     }
 }
 
-class UserInterface{
-    protected static void callUpUserInterface(){
+class UserInterface {
+    protected static void callUpUserInterface() {
         System.out.println();
         System.out.println("МЕНЮ");
         System.out.println("1 - посмотреть весь ассортимент");
@@ -107,21 +121,27 @@ class UserInterface{
         System.out.println();
     }
 
-    protected static String chooseAction(){
+    protected static String chooseAction() {
         Scanner scanner = new Scanner(System.in);
         String operation = "";
-        while (! (operation.equals("1") | operation.equals("2") | operation.equals("3"))) {
+        while (!(operation.equals("1") | operation.equals("2") | operation.equals("3"))) {
             System.out.println("Введите цифру с соответствующей операцией и подтвердите действие");
             operation = scanner.next();
         }
         return operation;
     }
 
-    //protected
+    protected static List<String> searchProducts() {
+        Scanner scanr = new Scanner(System.in);
+        System.out.println("Введите через пробел необходимые характеристики");
+
+        String[] paramArray = scanr.nextLine().split(" ");
+        return Arrays.asList(paramArray);
+    }
 }
 
-class Controller{
-    protected void runServer(){
+class Controller {
+    protected void runServer() {
         NotebookCollection notebookMap = Chop.initProducts();
 
         System.out.println();
@@ -132,10 +152,18 @@ class Controller{
             UserInterface.callUpUserInterface();
             String action = UserInterface.chooseAction();
 
-            switch (action){
-                case "1" -> notebookMap.printMapNotebook();
-                case "2" -> notebookMap.findNotebook();
-                case "3" -> flag = false;
+
+            switch (action) {
+                case "1" :
+                    notebookMap.printMapNotebook();
+                    break;
+                case "2" :
+                    List<String> listParameters = UserInterface.searchProducts();
+                    notebookMap.findNotebook(listParameters);
+                    break;
+                case "3" :
+                    flag = false;
+                    break;
             }
         }
     }
